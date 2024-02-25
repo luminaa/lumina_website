fe2b705d7b5a567427ec352c30eee88f1d36b594
Stan-breaks <stanleymwendwa03@gmail.com>
2024-02-25 12:50:04 +0300

    clean up

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
 
 //database
 require("./database");
